bc7f7b4baef42f22a3bbd3ff5c5e54ba8c8bdb41
Malcolm Lynch <milynch1320@gmail.com>
2019-06-11 23:24:07 -0500

    logger and validateId

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,4 +1,7 @@
-const express = 'express';
+const express = require('express');
+
+const Hubs = require('./userDb.js');
+const Messages = require('../posts/postDb.js');
 
 const router = express.Router();
 
@@ -32,9 +35,25 @@ router.put('/:id', (req, res) => {
 
 //custom middleware
 
-function validateUserId(req, res, next) {
-
-};
+function validateId(req, res, next) {
+    console.log("running the validate Id")
+    const { id } = req.params;
+    Hubs.findById(id)
+      .then(user => {
+        if (user) {
+          req.user = user;
+          next();
+        } else {
+          // We like very specific error messages
+          res.status(404).json({ message: "invalid user id" });
+        }
+      })
+      .catch(error => {
+        // log error to server
+        console.log(error);
+        res.status(500).json({ message: 'Failed to process request' });
+      });
+  }
 
 function validateUser(req, res, next) {
 
